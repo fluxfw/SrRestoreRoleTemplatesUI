4ed9c3d3d04103ef09feea89cc9a2f5105effb38
fluxfw <fluxfw@fluxlabs.ch>
2021-07-09 12:34:54 +0200

    Remove PHP7XBackport

--- vendor/composer/autoload_classmap.php
+++ vendor/composer/autoload_classmap.php
@@ -12,7 +12,4 @@ return array(
     'srag\\GeneratePluginInfosHelper\\SrRestoreRoleTemplatesUI\\GeneratePluginReadme' => $vendorDir . '/srag/generateplugininfoshelper/src/GeneratePluginReadme.php',
     'srag\\GeneratePluginInfosHelper\\SrRestoreRoleTemplatesUI\\UpdateVersion' => $vendorDir . '/srag/generateplugininfoshelper/src/UpdateVersion.php',
     'srag\\LibrariesNamespaceChanger\\LibrariesNamespaceChanger' => $vendorDir . '/srag/librariesnamespacechanger/src/LibrariesNamespaceChanger.php',
-    'srag\\LibrariesNamespaceChanger\\PHP72Backport' => $vendorDir . '/srag/librariesnamespacechanger/src/PHP72Backport.php',
-    'srag\\LibrariesNamespaceChanger\\PHP7Backport' => $vendorDir . '/srag/librariesnamespacechanger/src/PHP7Backport.php',
-    'srag\\LibrariesNamespaceChanger\\RemovePHP72Backport' => $vendorDir . '/srag/librariesnamespacechanger/src/RemovePHP72Backport.php',
 );
